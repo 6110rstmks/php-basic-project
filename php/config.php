76cdd465d034e6f83f5d0f5c5dfbf36f69bdb842
6110rstmks <92964241+6110rstmks@users.noreply.github.com>
2023-01-09 16:45:59 +0900

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,6 +1,10 @@
 
 <?php
 
+define('DSN', 'mysql:host=localhost;dbname=test;charset=utf8mb4');
+define('DB_USER', 'test');
+define('DB_PASS', '119089Meisei/');
+
 define("kakuningamen", "confirm_blade.php");
 
 define("kanryougamen", "register-complete_blade.php");  
