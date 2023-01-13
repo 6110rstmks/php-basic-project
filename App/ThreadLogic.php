32c46302d010ff986fa59205c9ff74fc0a39ae45
坂本碧 <c2131@oic.jp>
2023-01-13 19:40:43 +0900

    kadai4 comp

--- App/ThreadLogic.php
+++ App/ThreadLogic.php
@@ -35,15 +35,43 @@ class ThreadLogic {
     public function createThread(array $threadData)
     {
         // $result = false; 
-        $sql = 'INSERT INTO threads (title, content) VALUES (?, ?)';
+        // $sql = 'INSERT INTO threads (title, content, created_at) VALUES (?, ?, now())';
+        $sql = 'INSERT INTO threads (title, content, created_at) VALUES (:title, :content, now())';
 
         $arr = [];
 
-        $arr[] = $threadData['title'];
-        $arr[] = $threadData['comment'];
+        $title = $threadData['title'];
+
+        $comment = $threadData['comment'];
+
+
+
+        // $arr[] = $threadData['title'];
+        // $arr[] = $threadData['comment'];
+
+
 
         $stmt = $this->pdo->prepare($sql);
 
-        $stmt->execute($arr);
+        $stmt->bindValue('title', $title);
+        $stmt->bindValue('content', $comment);
+
+
+        // $stmt->execute($arr);
+        $stmt->execute();
+    }
+
+    public function searchThread($word)
+    {
+        $pattern = '%' . $word . '%'; 
+        $sql = "SELECT * FROM threads WHERE title LIKE :pattern ORDER BY created_at DESC";
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':pattern', $pattern);
+        $stmt->execute();
+
+        $threads = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $threads;
     }
 }
